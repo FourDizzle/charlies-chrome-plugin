d84bd943d32cc38f17261d4061b3e8c67abfd91a
FourDizzle <ndcassiani@gmail.com>
2020-03-22 13:07:17 -0500

    set image search default to false

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 const DEFAULT_QUERY = 'You\'ve been pranked!'
 const DEFAULT_METHOD = 'showResultsFor'
+const DEFAULT_IMAGE = false
 
 chrome.runtime.onInstalled.addListener(() => {
   setReplacementSearchQuery(DEFAULT_QUERY)
   setDisplayMethod(DEFAULT_METHOD)
+  setImageSearch(DEFAULT_IMAGE)
 })
